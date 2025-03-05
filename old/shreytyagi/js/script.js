e28be9c61ce4077f605873601851e3ed1557e730
shreytyagi <tyagi.shrey@gmail.com>
2025-03-05 18:09:17 +0530

    :wq
    :wq

---
+++ old/shreytyagi/js/script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const yearElement = document.getElementById("current-year");
+	const currentYear = new Date().getFullYear();
+	yearElement.textContent = "Copyright © "+currentYear+" by Shrey Tyagi. All rights reserved.";
+});
+
+/* document.addEventListener("DOMContentLoaded", function () {
+    const photoLink = document.querySelector(".photo-link");
+    const overlay = document.querySelector(".overlay");
+    const previewImage = document.querySelector(".preview-image");
+    const closeButton = document.querySelector(".close-btn");
+
+    photoLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        const imageURL = this.getAttribute("data-image");
+        previewImage.src = imageURL;
+        overlay.style.display = "block";
+    });
+
+    closeButton.addEventListener("click", function () {
+        overlay.style.display = "none";
+    });
+}); */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const photoLinks = document.querySelectorAll(".photo-link");
+    const overlay = document.querySelector(".overlay");
+    const previewImage = document.querySelector(".preview-image");
+    const closeButton = document.querySelector(".close-btn");
+
+    photoLinks.forEach(function (photoLink) {
+        photoLink.addEventListener("click", function (event) {
+            event.preventDefault();
+            const imageURL = this.getAttribute("data-image");
+            previewImage.src = imageURL;
+            overlay.style.display = "block";
+        });
+    });
+
+    closeButton.addEventListener("click", function () {
+        overlay.style.display = "none";
+    });
+});
+
+$(document).ready(function () {
+    $('.card').hover(
+        function () {
+            $(this).find('.card-caption').addClass('show-full');
+        },
+        function () {
+            $(this).find('.card-caption').removeClass('show-full');
+        }
+    );
+});
